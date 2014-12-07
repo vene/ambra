45a7a979d687b75851d3901171b826faa965389e
Vlad Niculae <vlad@vene.ro>
2014-12-07 18:39:54 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ambra',
+            version='0.1dev',
+            description='Temporal prediction by pairwise comparisons',
+            packages=['ambra'],
+)
+
